94aec0b13accddbe2e0e1b6cd01bcb0ecf290006
Thomas-Muller-20 <ssh-keygen -t rsa -b 4096 -C snipe.thomas.muller@gmail.com>
2024-11-19 22:31:27 +0200

    init commit

---
+++ src/main/java/com/bibliotrack/bibliotrackapi/BibliotrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotrack.bibliotrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotrackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bibliotrack/bibliotrackapi/BibliotrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotrack.bibliotrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotrackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
